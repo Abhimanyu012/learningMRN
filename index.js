b575a37230833a5689d569cf565ff56c1c9adbfa
Abhimanyu012 <abhimanyucode2021@gmail.com>
2025-08-16 23:10:29 +0530

    now i learn how to use the app.use("/api/notes") like this routing setup for separate route folder

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 import express from "express";
-
+import notesRoutes from"./routes/notesRoutes.js"
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "hello this is from / " });
-});
-app.post("/", (req, res) => {
-  res.status(201).json({ message: "hello this is from / " });
-});
 
-app.put("/", (req, res) => {
-  res.status(201).json({ message: "hello this is updated  from / " });
-});
+app.use("/api/notes",notesRoutes)
+
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "hello this is from / " });
+// });
+// app.post("/", (req, res) => {
+//   res.status(201).json({ message: "hello this is from / " });
+// });
+
+// app.put("/", (req, res) => {
+//   res.status(201).json({ message: "hello this is updated  from / " });
+// });
 
 app.listen(5001, () => [
   console.log("hello server is created and running successfully"),
