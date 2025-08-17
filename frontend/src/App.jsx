face90d7a214d051bbfe0b6fd58a35a69defb14a
Abhimanyu012 <abhimanyucode2021@gmail.com>
2025-08-17 19:22:34 +0530

    now frontend has been setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+  <div className=' bg-zinc-100 flex justify-center items-center h-screen w-full'>
+    <div className=' text-5xl font-bold'>
+      App</div>
+  
+  </div> )
+}
+
+export default App
